03afef80303ef3daaa71666eab44b58d7825d591
Zent <Zentrois@gmail.com>
2025-03-03 20:42:43 +0800

    9. Admin panel pages & layout settings

---
+++ application/views/layout/main_layout.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Dashboard</title>
+  <?php $this->load->view("include/styles") ?>
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+    <!-- Preloader -->
+    <div class="preloader flex-column justify-content-center align-items-center">
+        <img class="animation__shake" src="<?php echo base_url() ?>assets/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+    </div>
+
+    <!-- Navbar -->
+    <?php $this->load->view("include/navbar") ?>
+
+    <!-- Main Sidebar Container -->
+    <?php $this->load->view("include/sidebar") ?>
+
+    <!-- Content -->
+    <?php $this->load->view($page_content)?>
+
+    <!-- footer -->
+    <?php $this->load->view("include/footer") ?>
+  
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<?php $this->load->view('include/scripts') ?>
+</body>
+</html>
